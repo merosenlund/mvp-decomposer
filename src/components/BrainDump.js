07abd393f8d92678a684a2195d0869e546f1952b
Mitch Rosenlund <merosenlund@gmail.com>
2021-12-03 08:05:50 -0800

    onEnter listener added to the editor function

--- src/components/BrainDump.js
+++ src/components/BrainDump.js
@@ -4,8 +4,22 @@ import {Editor, EditorState} from 'draft-js';
 
 const BrainDump = () => {
   const [editorState, setEditorState] = useState(() => EditorState.createEmpty(),);
+
+  const onEnter = (e) => {
+    if (e.code === 'Enter') {
+      let contentState = editorState.getCurrentContent();
+      let lastBlock = contentState.getLastBlock();
+      let secondToLastBlock = contentState.getBlockBefore(lastBlock.getKey())
+      let text = secondToLastBlock.getText();
+      // console.log(text);
+      // debugger;
+    }
+  }
+
   return (
-    <Editor editorState={editorState} onChange={setEditorState} />
+    <div onKeyUp={onEnter}>
+      <Editor editorState={editorState} onChange={setEditorState}/>
+    </div>
   );
 };
 
