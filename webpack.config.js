09b68b5422744d75e75a894732e6016663365f51
Mitch Rosenlund <merosenlund@gmail.com>
2021-12-03 19:07:37 -0800

    Server set up and serving static files

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,30 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const htmlPlugin = new HtmlWebPackPlugin({
- template: "./src/index.html",
- filename: "./index.html"
+  template: "./src/index.html",
+  filename: "./index.html"
 });
+const path = require('path');
 module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
   devtool: 'source-map',
   mode: 'development',
-    module: {
-      rules: [{
-    test: /\.js$/,
-    exclude: /node_modules/,
-    use: {
-      loader: "babel-loader"
-    }
-  },
-    {
-    test: /\.css$/,
-    use: ["style-loader", "css-loader"]
-    }
-  ]},
-  plugins: [htmlPlugin],
-  {
-    test: /\.css$/,
-    use: [
-        { loader: 'style-loader' },
-        { loader: 'css-loader' },
-        { loader: 'less-loader' }
+  module: {
+    rules: [{
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
     ]
-  }
+  },
+  plugins: [htmlPlugin]
 };
